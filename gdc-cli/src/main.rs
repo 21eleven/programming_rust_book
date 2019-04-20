6d27795715becc54e3c994e7fd2d2b15a55d2f46
Noah Lidell <noahlidell@gmail.com>
2019-04-19 21:53:02 -0500

    working gdc cli

--- gdc-cli/src/main.rs
+++ gdc-cli/src/main.rs
@@ -1,3 +1,47 @@
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = vec![];
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+                        .expect("err parsing arg to u64"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...")
+                        .expect("err writing to stderr");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+
+    for n in numbers[1..].into_iter() {
+        d = gdc(d, *n);
+    }
+
+    println!("The greatest common divisor of {:?} is {}",
+            numbers, d);
+}
+
+fn gdc(mut x: u64, mut y: u64) -> u64 {
+    // to find gdc of two ints
+    // divide the bigger int by the smaller
+    // set the bigger of the two to equal the remainder
+    // you again have two ints
+    // loop until you get a remainder
+    // of zero
+    // return the remaining non zero int
+
+    assert!(x != 0 && y != 0);
+    while y != 0 {
+        if y < x {
+            let t = y;
+            y = x;
+            x = t;
+        }
+        y %= x;
+    }
+    x
 }
